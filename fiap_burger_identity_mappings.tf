0123ec68f445e1e2d1668ffbc7bf446f29f05487
gm50x <getuliomagela@outlook.com>
2024-09-24 20:00:29 -0300

    fix: incorrect remote state name

--- fiap_burger_identity_mappings.tf
+++ fiap_burger_identity_mappings.tf
@@ -10,7 +10,7 @@ data "terraform_remote_state" "fiap_burger_identity" {
 resource "aws_apigatewayv2_integration" "fiap_burger_identity_integration" {
   api_id           = aws_apigatewayv2_api.fiap_burger.id
   integration_type = "AWS_PROXY"
-  integration_uri  = data.terraform_remote_state.gateway.outputs.fiap_burger_identity_invoke_arn
+  integration_uri  = data.terraform_remote_state.fiap_burger_identity.outputs.fiap_burger_identity_invoke_arn
 }
 
 resource "aws_apigatewayv2_route" "proxy_handler" {
@@ -22,7 +22,7 @@ resource "aws_apigatewayv2_route" "proxy_handler" {
 resource "aws_lambda_permission" "api_gtw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = data.terraform_remote_state.gateway.outputs.fiap_burger_identity_function_name
+  function_name = data.terraform_remote_state.fiap_burger_identity.outputs.fiap_burger_identity_function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.fiap_burger.execution_arn}/*/*/identity"
 }
