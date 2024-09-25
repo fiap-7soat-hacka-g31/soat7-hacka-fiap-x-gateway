c74b0c3fcd06bd4d1e10bbde24a5fbfe6ce68aa0
gm50x <getuliomagela@outlook.com>
2024-09-25 19:35:14 -0300

    chore: adds fiap_burger_app eks mappings

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_account_id" {
+  default     = ""
+  description = "AWS Account Id"
+}
+
+variable "eks_loadbalance_listener_uri" {
+  default     = ""
+  description = "AWS Account Id"
+}
