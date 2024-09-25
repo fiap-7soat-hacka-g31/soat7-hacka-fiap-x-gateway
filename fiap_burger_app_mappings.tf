fcf6664a0f0e0de036c379da776935f62d590d4e
gm50x <getuliomagela@outlook.com>
2024-09-25 19:39:55 -0300

    fix: duplicated resource and incorrect remote state key

--- fiap_burger_app_mappings.tf
+++ fiap_burger_app_mappings.tf
@@ -9,7 +9,7 @@ data "terraform_remote_state" "fiap_burger_eks" {
 
 resource "aws_security_group" "vpc_link" {
   name   = "fiap-burger-k8s-vpc-link"
-  vpc_id = data.terraform_remote_state.fiap_burger_eks.fiap_burger_vpc_id
+  vpc_id = data.terraform_remote_state.fiap_burger_eks.outputs.fiap_burger_vpc_id
 
   egress {
     from_port   = 0
@@ -39,16 +39,6 @@ resource "aws_apigatewayv2_integration" "fiap_burger_eks" {
   integration_method = "ANY"
   connection_type    = "VPC_LINK"
   connection_id      = aws_apigatewayv2_vpc_link.fiap_burger_eks.id
-}
-
-resource "aws_apigatewayv2_integration" "fiap_burger_eks" {
-  api_id             = aws_apigatewayv2_api.fiap_burger.id
-  integration_uri    = var.eks_loadbalance_listener_uri
-  integration_type   = "HTTP_PROXY"
-  integration_method = "ANY"
-  connection_type    = "VPC_LINK"
-  connection_id      = aws_apigatewayv2_vpc_link.fiap_burger_eks.id
-  
 }
 
 resource "aws_apigatewayv2_route" "proxy" {
